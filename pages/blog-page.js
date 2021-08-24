f677aaba17412a1c577f2b2de4ac00fa1b9f5d06
taserbeat <taserbeat@gmail.com>
2021-08-24 20:42:14 +0900

    メインページを作成し、そこからリンクするブログページとタスクページのベースを作成

---
+++ pages/blog-page.js
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import Layout from '../components/Layout';
+
+export default function BlogPage() {
+  return (
+    <Layout title="Blog Page">
+      <Link href="/main-page">
+        <div className="flex cursor-pointer mt-12">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+            />
+          </svg>
+          <span>Back to main page</span>
+        </div>
+      </Link>
+    </Layout>
+  );
+}
