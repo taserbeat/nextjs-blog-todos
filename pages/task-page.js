5836de54bcfeead1a8e73bc5086f4388bfe75b61
taserbeat <taserbeat@gmail.com>
2021-09-05 17:38:03 +0900

    タスクの閲覧機能を作成

--- pages/task-page.js
+++ pages/task-page.js
@@ -1,9 +1,38 @@
+import { useEffect } from 'react';
 import Link from 'next/link';
-import Layout from '../components/Layout';
+import useSWR from 'swr';
 
-export default function TaskPage() {
+import Layout from '../components/Layout';
+import { getAllTasksData } from '../lib/tasks';
+import Task from '../components/Task';
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
+const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}/api/list-task/`;
+
+export default function TaskPage({ staticTasks }) {
+  // 初回のアクセス時はinitialDataに設定した値がセットされる
+  // mutateはキャッシュを更新するコールバックである
+  const { data: tasks, mutate } = useSWR(apiUrl, fetcher, {
+    initialData: staticTasks,
+  });
+
+  const sortedTasks = tasks?.sort(
+    (a, b) => new Date(b.created_at) - new Date(a.created_at)
+  );
+
+  useEffect(() => {
+    // useSWRで取得したキャッシュを最新のものにする
+    mutate();
+  }, [mutate]);
+
   return (
     <Layout title="Task Page">
+      {/* タスクのリスト */}
+      <ul>
+        {sortedTasks &&
+          sortedTasks.map((task) => <Task key={task.id} task={task} />)}
+      </ul>
+
       <Link href="/main-page" passHref>
         <div className="flex cursor-pointer mt-12">
           <svg
@@ -25,4 +54,14 @@ export default function TaskPage() {
       </Link>
     </Layout>
   );
+}
+
+// ビルド時(yarn build)に呼ばれる
+export async function getStaticProps() {
+  const staticTasks = await getAllTasksData();
+
+  return {
+    props: { staticTasks },
+    revalidate: 3,
+  };
 }
