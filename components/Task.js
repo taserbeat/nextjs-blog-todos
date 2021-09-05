f92b2f28a658157382547122c68b0b206c8f5cc8
taserbeat <taserbeat@gmail.com>
2021-09-05 20:33:09 +0900

    タスクの作成・更新処理

--- components/Task.js
+++ components/Task.js
@@ -1,9 +1,14 @@
+import { useContext } from 'react';
 import Link from 'next/link';
 import Cookie from 'universal-cookie';
+
+import { StateContext } from '../context/StateContext';
 
 const cookie = new Cookie();
 
 export default function Task({ task, taskDeleted }) {
+  const { setSelectedTask } = useContext(StateContext);
+
   const deleteTask = async () => {
     await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}/api/tasks/${task.id}`, {
       method: 'DELETE',
@@ -29,10 +34,31 @@ export default function Task({ task, taskDeleted }) {
           {task.title}
         </span>
       </Link>
-      <div className="float-right ml-20 cursor-pointer">
+
+      <div className="float-right ml-20">
+        {/* 編集ボタン */}
+        <svg
+          onClick={() => {
+            setSelectedTask(task);
+          }}
+          className="h-6 w-6 float-left cursor-pointer"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+          />
+        </svg>
+
+        {/* 削除ボタン */}
         <svg
           onClick={deleteTask}
-          className="h-6 w-6"
+          className="h-6 w-6 mr-2 cursor-pointer"
           fill="none"
           stroke="currentColor"
           viewBox="0 0 24 24"
--- pages/task-page.js
+++ pages/task-page.js
@@ -5,6 +5,8 @@ import useSWR from 'swr';
 import Layout from '../components/Layout';
 import { getAllTasksData } from '../lib/tasks';
 import Task from '../components/Task';
+import StateContextProvider from '../context/StateContext';
+import TaskForm from '../components/TaskForm';
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
 const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}/api/list-task/`;
@@ -26,35 +28,40 @@ export default function TaskPage({ staticTasks }) {
   }, [mutate]);
 
   return (
-    <Layout title="Task Page">
-      {/* タスクのリスト */}
-      <ul>
-        {sortedTasks &&
-          sortedTasks.map((task) => (
-            <Task key={task.id} task={task} taskDeleted={mutate} />
-          ))}
-      </ul>
+    <StateContextProvider>
+      <Layout title="Task Page">
+        {/* タスクの入力フォーム */}
+        <TaskForm taskCreated={mutate} />
 
-      <Link href="/main-page" passHref>
-        <div className="flex cursor-pointer mt-12">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
-            />
-          </svg>
-          <span>Back to main page</span>
-        </div>
-      </Link>
-    </Layout>
+        {/* タスクのリスト */}
+        <ul>
+          {sortedTasks &&
+            sortedTasks.map((task) => (
+              <Task key={task.id} task={task} taskDeleted={mutate} />
+            ))}
+        </ul>
+
+        <Link href="/main-page" passHref>
+          <div className="flex cursor-pointer mt-12">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+              />
+            </svg>
+            <span>Back to main page</span>
+          </div>
+        </Link>
+      </Layout>
+    </StateContextProvider>
   );
 }
 
